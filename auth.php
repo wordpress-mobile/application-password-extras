27c3119b94597a7cdac3bf184ae282038799698a
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-09-11 16:36:57 -0600

    Advertise extras support in the API

--- auth.php
+++ auth.php
@@ -18,4 +18,24 @@ function application_password_extras($original_value) {
 	return $original_value;
 }
 
-add_filter('application_password_is_api_request', 'application_password_extras', 100);
+add_filter( 'application_password_is_api_request', 'application_password_extras' );
+
+// Add an endpoint to advertise support. This could also be setting that's only registered
+// for the REST API, but having a way to know upfront whether this will work or not is 
+// really helpful for the application UI.
+function application_password_extra_capabilities() {
+	return [
+		'admin-ajax',
+		'post-previews'
+	];
+}
+
+add_action( 'rest_api_init', function() {
+	register_rest_route( 'application-password-extras/v1', 'capabilities', [
+		'methods'  				=> WP_REST_Server::READABLE,
+		'callback' 				=> 'application_password_extra_capabilities',
+		'permission_callback' 	=> 'is_user_logged_in',
+	] );
+});
+
+
